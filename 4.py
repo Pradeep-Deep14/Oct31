b8f7e90872fbbd4a7415c91d9e5e752ca888f118
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-10-31 18:32:04 +0530

    first commit

---
+++ 4.py
@@ -0,0 +1,9 @@
+m=[4,2,3,1,2]
+m.remove(2)
+print(m)
+
+
+#[4,3,1,2]
+#Python  remove function, 
+#removes the first occurrence of the integer 2 from the list m. 
+# The resulting list will be [4, 3, 1, 2]
